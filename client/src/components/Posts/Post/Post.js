b3c4c4e28b4967e2538a3cbdd5d0d1910e92741a
bhagath vikas <bhagathvikas@gmail.com>
2022-06-09 23:28:39 +0530

    creating post func

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,28 @@
 import React from "react";
 import useStyles from './style';
+import {Card,CardAction,CArdContent,CardMedia,Button,Typography} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/core/ThumbUpAlt';
+import DelteIcon from '@material-ui/core/Delete';
+import MoreHorizIcon from '@material-ui/core/MoreHorizIcon'
 
  const Post = ()=>{
     const classes = useStyles();
      return(
-         <h1>Post</h1>
+         <Card className={classes.card} >
+             <CardMedia className={classes.media} image ={post.selectedFile} title={post.title}/>
+             <div className={classes.overlay}>
+                 <Typography variant="h6">{post.newPost}</Typography>
+                 {/* //<Typography variant="body2">{SVGAnimateMotionElement(po)}</Typography> */}
+
+
+             </div>
+             <div className={classes.overlay2}>
+                 <Button style={{color:'white'}} size="small" onClick={}></Button>
+
+
+             </div>
+
+         </Card>
      );
  }
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,31 @@
-import React from "react";
+
 import { useSelector } from "react-redux";
 import Post from '../Posts/Post/Post.js';
-import useStyles from './style';
 
+import React from "react";
+import useStyles from './style';
+import {Grid,CircularProgress} from '@material-ui/core';
 
  const Posts = ()=>{
      const posts = useSelector((state)=>state.posts);
     const classes = useStyles();
-    console.log(posts);
      return(
-         <>
-         <h1>Posts</h1>
-         <Post/>
-         <Post/>
-         </>
+         !posts.length ? <CircularProgress/>:(
+             <Grid className={classes.container} container alignitems="strecth" spacing="3">
+                 {
+                     posts.map((post)=>(
+                         <Grid key={post._id} items xs={12} sm={6}
+                        >
+                            <Post post={post}/>
+
+                         </Grid>
+                     
+                     ))
+                 }
+
+             </Grid>
+         )
      );
  }
 
- export default Posts;
+ export default Post;
