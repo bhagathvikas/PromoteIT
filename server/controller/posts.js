c6a83e2911e9e83ff856d73280c67d101ed05061
bhagath vikas <bhagathvikas@gmail.com>
2022-06-13 01:19:11 +0530

    upadte post fixed

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -8,7 +8,6 @@ export const getPost = async(req,res) => {
 
     try {
         const postMessages = await PostMessage.find();
-                
         res.status(200).json(postMessages);
         
     } catch (error) {
