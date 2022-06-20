0a91333b6e83eaecdb840c4be391221bf87749d6
bhagath vikas <bhagathvikas@gmail.com>
2022-06-21 00:11:46 +0530

    pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route,Navigate  } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar'
 import Home from  './components/home/Home'
-import Auth from './components/Auth/Auth.js'
+import AUTH from './components/Auth/Auth.js'
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = ()=>{
+
+  const user = JSON.parse(localStorage.getItem('profile'))
+  
+  
   
   return(
     <BrowserRouter>
-    <Container maxWidth="lg">
+    <Container maxWidth="xl">
       <NavBar />
       <Routes>
-        <Route path="/" exact element={<Home/>} />
-        <Route path="/auth" exact element={<Auth/>} />
+        <Route path="/"  element={<Navigate replace to="/posts"/>} />
+        <Route path="/posts"  element = {<Home/>}/>
+        <Route path="/posts/search"  element = {<Home/>}/>
+        <Route path="/posts/:id"  element = {<PostDetails/>}/>
+        
+        <Route
+        path="/auth"
+        element={!user ? <AUTH /> : <Navigate replace to="/posts" />}
+      />
       </Routes>
     </Container>
   </BrowserRouter>
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,13 +1,16 @@
 import React,{useState,useEffect} from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid,Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Forms';
 import {useDispatch} from 'react-redux';
 import {getPosts} from '../../actions/posts';
+import Paginatation from '../Pagination/PagiNation'
+import useStyles from './style'
 const Home = () => {
     const [currentId,setCurrentId] = useState(null);
  
   const dispatch = useDispatch();
+  const classes = useStyles()
 
   useEffect(()=>{
     dispatch(getPosts());
@@ -19,6 +22,10 @@ const Home = () => {
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId}  />
             </Grid>
+            <Paper className={classes.Paper} elevation= {6}>
+              <Paginatation/>
+
+            </Paper>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
